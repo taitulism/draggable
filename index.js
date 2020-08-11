47f03c3b1ad0cac05fef1d09a694f9ef9a207aa8
Itai Tenenbaum <taitulism@gmail.com>
2020-08-11 10:48:27 +0300

    The Big Bang

--- index.js
+++ index.js
@@ -1,3 +1,52 @@
-const TPL = require('./src/TPL');
+// const TPL = require('./src/TPL');
 
-module.exports = TPL;
+module.exports = function draggable (elm, opts) {
+	return new Draggable(elm, opts);
+};
+
+function Draggable (elm, opts) {
+	this.elm = elm;
+	this.startMouseX = 0;
+	this.elmX = 0;
+	this.events = {dragStart: [], drop: [], dragging: []};
+
+	this.elm.style.position = 'relative';
+
+	this.onDragStart = this.onDragStart.bind(this);
+	this.onDragging = this.onDragging.bind(this);
+	this.onDrop = this.onDrop.bind(this);
+
+	elm.addEventListener('mousedown', this.onDragStart);
+	elm.addEventListener('mouseup', this.onDrop);
+}
+
+Draggable.prototype.on = function (eventName, callback) {
+	// this.events[eventName] = this.events[eventName] || [];
+	this.events[eventName].push(callback);
+};
+
+Draggable.prototype.onDragStart = function (ev) {
+	this.startMouseX = ev.clientX;
+	this.elmX = extractNumber(this.elm.style.left);
+
+	document.addEventListener('mousemove', this.onDragging);
+	this.events.dragStart.forEach(cb => cb());
+};
+
+Draggable.prototype.onDragging = function (ev) {
+	const mouseMovedX = ev.clientX - this.startMouseX;
+
+    // eslint-disable-next-line max-len
+    // console.log(`${this.elmX} + ${ev.clientX - this.startMouseX} (${ev.clientX} - ${this.startMouseX})`);
+	this.elm.style.left = this.elmX + mouseMovedX  + 'px';
+	this.events.dragging.forEach(cb => cb(ev));
+};
+
+Draggable.prototype.onDrop = function (ev) {
+	document.removeEventListener('mousemove', this.onDragging);
+	this.events.drop.forEach(cb => cb());
+};
+
+function extractNumber (rawValue) {
+	return parseInt(rawValue || 0, 10);
+}
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,12 +1,125 @@
-const chai = require('chai');
-const chaiAsPromised = require('chai-as-promised');
-const expect = chai.expect;
-chai.use(chaiAsPromised);
+const {expect} = require('chai');
+const jsdom = require('jsdom');
 
-const TPL = require('../');
+const { JSDOM } = jsdom;
 
-describe('TPL', () => {
-	it('is ok', () => {
-		expect(TPL).to.be.ok;
+const draggable = require('../');
+
+const setWinDoc = (dom) => {
+	global.window = dom.window;
+	global.document = dom.window.document;
+};
+
+const createEvent = (type, props = {}) => {
+	const event = new global.window.Event(type, {bubbles: true});
+	Object.assign(event, props);
+	return event;
+};
+
+function simulateMouseDown (elm, x, y) {
+	const event = createEvent('mousedown', {
+		clientX: x || 0,
+		clientY: y || 0,
+	});
+	elm.dispatchEvent(event);
+}
+
+function simulateMouseMove (elm, x, y) {
+	const event = createEvent('mousemove', {
+		clientX: x || 0,
+		clientY: y || 0,
+	});
+
+	elm.dispatchEvent(event);
+}
+
+function simulateMouseUp (elm, x, y) {
+	const event = createEvent('mouseup', {
+		clientX: x || 0,
+		clientY: y || 0,
+	});
+
+	elm.dispatchEvent(event);
+}
+
+describe('draggable', () => {
+	let container, target;
+	beforeEach(() => JSDOM.fromFile('./tests/test.html').then((dom) => {
+		setWinDoc(dom);
+		container = document.getElementById('container');
+		target = document.getElementById('target');
+	}));
+
+	afterEach(() => {
+		global.window.close();
+		target = null;
+		container = null;
+		global.window = null;
+		global.document = null;
+	});
+
+	it('is a function', () => expect(draggable).to.be.a('function'));
+
+	it('returns a draggable instance', () => {
+		const draggableInstance = draggable(target);
+		const ctor = Object.getPrototypeOf(draggableInstance).constructor;
+
+		expect(ctor.name).to.equal('Draggable');
+	});
+
+	it('emits onDragStart', () => {
+		const drg = draggable(target);
+		let fired = false;
+
+		drg.on('dragStart', () => {
+			fired = true;
+		});
+
+		simulateMouseDown(target, 50, 50);
+
+		expect(fired).to.be.true;
+	});
+
+	it('emits onDrop', () => {
+		const drg = draggable(target);
+		let fired = false;
+
+		drg.on('drop', () => {
+			fired = true;
+		});
+
+		simulateMouseDown(target, 50, 50);
+		simulateMouseUp(target, 50, 50);
+
+		expect(fired).to.be.true;
+	});
+
+	it('emits dragging', () => {
+		const drg = draggable(target);
+		let fired = false;
+
+		drg.on('dragging', () => {
+			fired = true;
+		});
+
+		simulateMouseDown(target, 50, 50);
+		simulateMouseMove(target, 50, 50);
+		simulateMouseUp(target, 50, 50);
+
+		expect(fired).to.be.true;
+	});
+
+	it('moves the elm', () => {
+		draggable(target);
+		expect(target.style.left).to.be.empty;
+
+		simulateMouseDown(target, 50, 50);
+
+		simulateMouseMove(target, 50, 50);
+		expect(target.style.left).to.equal('0px');
+
+		simulateMouseMove(target, 150, 50);
+		expect(target.style.left).to.equal('100px');
 	});
 });
+
