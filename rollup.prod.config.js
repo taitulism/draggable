90c03b5992ea1dfd47211c5c862766cc043283ae
Itai Tenenbaum <taitulism@gmail.com>
2020-10-07 15:46:51 +0300

    infra: bundle names, pksj prepare script, pkjs files prop

--- rollup.prod.config.js
+++ rollup.prod.config.js
@@ -1,14 +1,24 @@
+// import {terser} from 'rollup-plugin-terser';
+
 export default [{
 	input: 'draggable.js',
 	output: {
-		file: 'dist/draggable.esm-bundle.js',
+		file: 'dist/draggable.esm.js',
 		format: 'es',
 	},
-},{
+}, {
 	input: 'draggable.js',
 	output: {
 		file: 'dist/draggable.browser.js',
 		format: 'iife',
 		name: 'draggable',
-	},
+	}
+// }, {
+// 	input: 'draggable.js',
+// 	plugins: [terser()],
+// 	output: {
+// 		file: 'dist/draggable.browser.min.js',
+// 		format: 'iife',
+// 		name: 'draggable',
+// 	},
 }];
