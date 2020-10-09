4cc81410903a40741f5e1cefd89a2449f667b32b
Itai Tenenbaum <taitulism@gmail.com>
2020-10-10 00:36:14 +0300

    fix prod script

--- rollup.prod.config.js
+++ rollup.prod.config.js
@@ -1,24 +1,31 @@
-// import {terser} from 'rollup-plugin-terser';
 
 export default [{
-	input: 'draggable.js',
+	input: 'index.js',
 	output: {
 		file: 'dist/draggable.esm.js',
 		format: 'es',
 	},
 }, {
-	input: 'draggable.js',
+	input: 'index.js',
 	output: {
 		file: 'dist/draggable.browser.js',
 		format: 'iife',
 		name: 'draggable',
 	}
-// }, {
-// 	input: 'draggable.js',
-// 	plugins: [terser()],
-// 	output: {
-// 		file: 'dist/draggable.browser.min.js',
-// 		format: 'iife',
-// 		name: 'draggable',
-// 	},
 }];
+
+/*
+minify:
+-------
+	import {terser} from 'rollup-plugin-terser';
+
+	...
+
+	input: 'index.js',
+	plugins: [terser()],
+	output: {
+		file: 'dist/draggable.browser.min.js',
+		format: 'iife',
+		name: 'draggable',
+	},
+*/
